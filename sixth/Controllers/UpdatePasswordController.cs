43ce8bd4398ce502a03ba05ec152d1d97752cf84
rohit <rohit.sahu@connexistech.com>
2021-09-11 22:01:24 +0530

    All Login and Registration is completed

---
+++ sixth/Controllers/UpdatePasswordController.cs
@@ -0,0 +1,45 @@
+﻿using sixth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace sixth.Controllers
+{
+    public class UpdatePasswordController : ApiController
+    {
+        public IHttpActionResult Post([FromBody] update_password password)
+        {
+            using (needDbEntities needDbEntities = new needDbEntities())
+            {
+                try
+                {
+                    var emailCheck = needDbEntities.need_login_table.Where(e => e.email == password.email).FirstOrDefault();
+
+                    if (password == null || password.password.Length < 8)
+                    {
+                        return BadRequest("Password cannot be Empty or less than 8 character");
+                    }
+
+                    if (emailCheck != null)
+                    {
+                        emailCheck.password = password.password;
+                        needDbEntities.SaveChanges();
+                        var data = needDbEntities.need_login_table.FirstOrDefault(e => e.email == password.email);
+                        return Ok(data);
+                    }
+                    else
+                    {
+                        return BadRequest("Somthing went Wrong!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Somthing went Wrong!" + e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ sixth/Models/update_password.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sixth.Models
+{
+    public class update_password
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
